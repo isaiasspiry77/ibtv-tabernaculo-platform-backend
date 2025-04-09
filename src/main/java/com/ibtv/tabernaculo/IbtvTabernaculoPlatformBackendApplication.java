14e980a87d70ed7cb2733a523f2c472d43e24ae3
isaias.espiridion <isaias.espiridion@logicinfo.com>
2025-04-09 17:16:49 -0600

    backend project

---
+++ src/main/java/com/ibtv/tabernaculo/IbtvTabernaculoPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ibtv.tabernaculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbtvTabernaculoPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbtvTabernaculoPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibtv/tabernaculo/IbtvTabernaculoPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibtv.tabernaculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbtvTabernaculoPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
